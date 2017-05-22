7a21cb8c97dea6ded4bc6d0226850774206e6648
michal <michal@sparkbit.pl>
2017-05-23 00:00:01 +0200

    Added pacakge for Jackson classes, eg. serializers
    Change-Id: I1aa2bb4dcbc0d739cbc30cc467249ff48e9c951e

---
+++ src/main/java/pl/sparkbit/commons/jackson/InstantAsMillisSerializer.java
@@ -0,0 +1,21 @@
+package pl.sparkbit.commons.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.Instant;
+
+public class InstantAsMillisSerializer extends JsonSerializer<Instant> {
+
+    @Override
+    public void serialize(Instant value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+        if (null == value) {
+            jgen.writeNull();
+        } else {
+            long millis = value.toEpochMilli();
+            jgen.writeNumber(millis);
+        }
+    }
+}
