f94c2cdd334390dfc8dbcbc0d6229edfe0163224
michal <michal@sparkbit.pl>
2017-05-30 09:05:07 +0200

    Added DurationTypeHandler
    Change-Id: I76ba9f45292b1004ef3250a66251c6fe5354335e

---
+++ src/main/java/pl/sparkbit/commons/mybatis/DurationTypeHandler.java
@@ -0,0 +1,38 @@
+package pl.sparkbit.commons.mybatis;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Duration;
+
+@SuppressWarnings("unused")
+public class DurationTypeHandler extends BaseTypeHandler<Duration> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Duration duration, JdbcType jdbcType)
+            throws SQLException {
+        ps.setLong(i, duration.toMillis());
+    }
+
+    @Override
+    public Duration getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        long duration = rs.getLong(columnName);
+        return Duration.ofMillis(duration);
+    }
+
+    @Override
+    public Duration getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        long duration = rs.getLong(columnIndex);
+        return Duration.ofMillis(duration);
+    }
+
+    @Override
+    public Duration getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        long duration = cs.getLong(columnIndex);
+        return Duration.ofMillis(duration);
+    }
+}
