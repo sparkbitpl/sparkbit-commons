cf519ba90ef9d7ee22b959fab431c314280d956a
michal <michal@sparkbit.pl>
2017-06-15 21:57:29 +0200

    Added rest message logger
    Change-Id: I9f6d37a8ea56c0feaee3f96ceabb49fc1f45a84e

---
+++ src/main/java/pl/sparkbit/commons/restlogger/RestLoggingFilter.java
@@ -0,0 +1,92 @@
+package pl.sparkbit.commons.restlogger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.InvalidMediaTypeException;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class RestLoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger("restlogger");
+
+    private final AtomicLong id = new AtomicLong(0);
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        long requestId = id.incrementAndGet();
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("{} Incoming request headers \n{}", inPrompt(requestId), getHeaders(requestId, request));
+            try {
+                if (request.getContentType() != null &&
+                        MediaType.valueOf(request.getContentType()).isCompatibleWith(MediaType.APPLICATION_JSON)) {
+                    request = new RequestWrapper(request, inPrompt(requestId));
+                    LOG.trace(((RequestWrapper) request).asString());
+                }
+            } catch (InvalidMediaTypeException e) {
+                LOG.trace("Request has invalid Content-Type!");
+            }
+
+            response = new ResponseWrapper(response);
+            chain.doFilter(request, response);
+
+            LOG.trace("{} Outgoing response headers (possibly not all included): \n{}", outPrompt(requestId),
+                    getHeaders(requestId, response, request.getProtocol()));
+            if (response.getContentType() != null &&
+                    MediaType.valueOf(response.getContentType()).isCompatibleWith(MediaType.APPLICATION_JSON)) {
+                LOG.trace(((ResponseWrapper) response).asString(outPrompt(requestId)));
+            }
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    private String getHeaders(long requestId, HttpServletRequest request) {
+        String prompt = inPrompt(requestId);
+        StringBuilder logBuilder = new StringBuilder();
+        logBuilder.append(prompt).append(request.getMethod()).append(' ').append(request.getRequestURI());
+        if (request.getQueryString() != null) {
+            logBuilder.append('?').append(request.getQueryString());
+        }
+        logBuilder.append(' ').append(request.getProtocol()).append('\n');
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            logBuilder.append(prompt).append(headerName).append(" : ").
+                    append(request.getHeader(headerName)).append('\n');
+        }
+        return logBuilder.deleteCharAt(logBuilder.length() - 1).toString();
+    }
+
+    private String getHeaders(long requestId, HttpServletResponse response, String protocol) {
+        String prompt = outPrompt(requestId);
+        StringBuilder logBuilder = new StringBuilder();
+        logBuilder.append(prompt).append(protocol).append(' ').append(response.getStatus()).append(' ').
+                append(HttpStatus.valueOf(response.getStatus()).name()).append('\n');
+        Collection<String> headerNames = response.getHeaderNames();
+        for (String headerName : headerNames) {
+            logBuilder.append(prompt).append(headerName).append(" : ").append(response.getHeader(headerName)).
+                    append('\n');
+        }
+        return logBuilder.deleteCharAt(logBuilder.length() - 1).toString();
+    }
+
+    private String outPrompt(long requestId) {
+        return requestId + " <== ";
+    }
+
+    private String inPrompt(long requestId) {
+        return requestId + " ==> ";
+    }
+}
---
+++ src/main/java/pl/sparkbit/commons/restlogger/TeePrintWriter.java
@@ -0,0 +1,32 @@
+package pl.sparkbit.commons.restlogger;
+
+import java.io.PrintWriter;
+
+public class TeePrintWriter extends PrintWriter {
+
+    private final PrintWriter branch;
+
+    TeePrintWriter(PrintWriter writer, PrintWriter branch) {
+        super(writer);
+        this.branch = branch;
+    }
+
+    @Override
+    public synchronized void write(char[] buf, int off, int len) {
+        super.write(buf, off, len);
+        branch.write(buf, off, len);
+        branch.flush();
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        branch.close();
+    }
+
+    @Override
+    public void flush() {
+        super.flush();
+        branch.flush();
+    }
+}
