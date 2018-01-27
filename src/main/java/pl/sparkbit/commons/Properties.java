336ed24a86756b3264d0f8ad104136e733dfb489
michal <michal@sparkbit.pl>
2018-01-27 21:55:44 +0100

    scs-17 Add json logging functionality (spring config) to commons
    Change-Id: I2f05189fe23b3fbf4b9dd9ec9b23b0b1d7f72864

--- src/main/java/pl/sparkbit/commons/Properties.java
+++ src/main/java/pl/sparkbit/commons/Properties.java
@@ -1,5 +1,10 @@
 package pl.sparkbit.commons;
 
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
 @SuppressWarnings("checkstyle:hideutilityclassconstructor")
 public class Properties {
 
@@ -7,6 +12,7 @@ public class Properties {
     public static final String CLOCK_ENABLED = "sparkbit.commons.clock.enabled";
     public static final String ID_GENERATOR_ENABLED = "sparkbit.commons.idGenerator.enabled";
     public static final String MYBATIS_METRICS_ENABLED = "sparkbit.commons.mybatisMetrics.enabled";
+    public static final String REQUEST_LOGGING_ENABLED = "sparkbit.commons.requestLogging.enabled";
     public static final String STATSD_ENABLED = "sparkbit.commons.statsd.enabled";
     public static final String STATSD_HOST = "sparkbit.commons.statsd.host";
     public static final String STATSD_PORT = "sparkbit.commons.statsd.port";
---
+++ src/main/java/pl/sparkbit/commons/restlogger/RequestLoggingConfiguration.java
@@ -0,0 +1,32 @@
+package pl.sparkbit.commons.restlogger;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+import static pl.sparkbit.commons.Properties.REQUEST_LOGGING_ENABLED;
+
+@ConditionalOnProperty(value = REQUEST_LOGGING_ENABLED, havingValue = "true", matchIfMissing = true)
+@Configuration
+public class RequestLoggingConfiguration {
+
+    private static final Integer RIGHT_AFTER_HIGHEST_PRECEDENCE = HIGHEST_PRECEDENCE + 1;
+
+    @Bean
+    public FilterRegistrationBean requestLoggingFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(requestLoggingFilter());
+        //this filter should run very early in the chain - possibly only after compression filter
+        registration.setOrder(RIGHT_AFTER_HIGHEST_PRECEDENCE);
+        return registration;
+    }
+
+    @Bean
+    public Filter requestLoggingFilter() {
+        return new RestLoggingFilter();
+    }
+}
