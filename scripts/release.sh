3014a6332606ff5afcc1740048600a5e2aa21470
Michał Kręglewski <michal@sparkbit.pl>
2020-08-24 12:57:01 +0200

    Clean workspace before deploy
    Change-Id: I8045fda7b74bfdaaf4ce94a01c16327b1d6df7e3

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,7 @@ export projectName=sparkbit-commons
 cd $repoRoot
 
 scripts/bump_version.sh
-mvn -s "${NEXUS_SETTINGS_XML}" -P release -DskipTests -Dcheckstyle.skip -Dmaven.antrun.skip deploy
+mvn -s "${NEXUS_SETTINGS_XML}" -P release -DskipTests -Dcheckstyle.skip -Dmaven.antrun.skip clean deploy
 
 git commit -a -m "RELEASE: release $projectName-$RELEASE_VERSION"
 git tag -am "$projectName-$RELEASE_VERSION" $projectName-$RELEASE_VERSION
